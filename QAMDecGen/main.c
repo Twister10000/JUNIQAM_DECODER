33edb7b6b7ae0378d5d37d73bf1ee0dc43587fef
Twister10000 <82016318+Twister10000@users.noreply.github.com>
2023-11-28 20:37:05 +0100

    add more stack

--- QAMDecGen/main.c
+++ QAMDecGen/main.c
@@ -56,7 +56,7 @@ int main(void)
 	initDecDMA();
 	
 	xTaskCreate(vQuamGen, NULL, configMINIMAL_STACK_SIZE+500, NULL, 2, NULL);
-	xTaskCreate(vQuamDec, NULL, configMINIMAL_STACK_SIZE+300, NULL, 1, NULL);
+	xTaskCreate(vQuamDec, NULL, configMINIMAL_STACK_SIZE+400, NULL, 1, NULL);
 
 	vDisplayClear();
 	vDisplayWriteStringAtPos(0,0,"FreeRTOS 10.0.1");
