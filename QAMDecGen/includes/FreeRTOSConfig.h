5a02ee0c6d09314b0c14ddd52c9c493df020619b
Twister10000 <82016318+Twister10000@users.noreply.github.com>
2023-12-05 21:15:24 +0100

    added defines for Semaphores

--- QAMDecGen/includes/FreeRTOSConfig.h
+++ QAMDecGen/includes/FreeRTOSConfig.h
@@ -82,6 +82,8 @@
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			1
 #define configCHECK_FOR_STACK_OVERFLOW	2
+#define configSUPPORT_DYNAMIC_ALLOCATION 1
+#define configUSE_MUTEXES				1
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES 		1
