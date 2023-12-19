aedcf9843ece0cc541422e55e4c0d80c8879e8a8
Twister10000 <82016318+Twister10000@users.noreply.github.com>
2023-12-19 14:55:41 +0100

    added code for I2C

--- QAMDecGen/main.c
+++ QAMDecGen/main.c
@@ -58,7 +58,7 @@ int main(void)
 	
 	LSM9DS1Init();
 	
-/*	xTaskCreate(vQuamGen, NULL, configMINIMAL_STACK_SIZE+500, NULL, 2, NULL);*/
+	/*xTaskCreate(vQuamGen, NULL, configMINIMAL_STACK_SIZE+500, NULL, 2, NULL);*/
 	xTaskCreate(vQuamDec, NULL, configMINIMAL_STACK_SIZE+400, NULL, 2, NULL);
 	xTaskCreate(vTest, NULL, configMINIMAL_STACK_SIZE+400, NULL, 1, NULL);
 
--- QAMDecGen/qamgen.c
+++ QAMDecGen/qamgen.c
@@ -151,7 +151,7 @@ void vQuamGen(void *pvParameters) {
 		if (new_time - old_time >= 1000)
 		{
 			 				readTempData();
-			 				temparatur =  37.54;//getTemperatureData();
+			 				temparatur =  getTemperatureData();
 			 				printBinary(byteArray[4]);
 			 				createBinary();
 							old_time = new_time;
